f56f0800f5728bb6a6167289c72a0d9d48d0072b
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-14 13:30:52 -0600

    created a class create nav bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import './style.css';
+import navItem from './navClass';
 
 const navigationBar = document.querySelectorAll('.navigationBar');
 
-navBarItems = [];
+let navBarContent = {
+  title0: new navItem('title0', 'item0', 'item1', 'item2', 'item3'),
+  title1: new navItem('title1', 'item0', 'item1', 'item2', 'item3'),
+  title2: new navItem('title2', 'item0', 'item1', 'item2', 'item3'),
+  title3: new navItem('title3', 'item0', 'item1', 'item2', 'item3'),
+};
---
+++ src/navClass.js
@@ -0,0 +1,15 @@
+export default class navItem {
+  constructor(
+    dropdownTitle,
+    dropdownItem0,
+    dropdownItem1,
+    dropdownItem2,
+    dropdownItem3
+  ) {
+    this.dropdownTitle = dropdownTitle;
+    this.dropdownItem0 = dropdownItem0;
+    this.dropdownItem1 = dropdownItem1;
+    this.dropdownItem2 = dropdownItem2;
+    this.dropdownItem3 = dropdownItem3;
+  }
+}
