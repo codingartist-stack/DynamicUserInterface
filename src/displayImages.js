ad03242f55b82e8bdcfb64da6c7478e11240a233
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-16 19:40:43 -0600

    trying to add create a loop to display the images... not sure how to do it

---
+++ src/displayImages.js
@@ -0,0 +1,31 @@
+//images
+import architecture from './images/architecture1920.jpg';
+import castelSantangel from './images/castelSantangelo1920.jpg';
+import fairyTale from './images/fairytale1920.jpg';
+import neuschwanstein from './images/neuschwanstein1920.jpg';
+import waterCastel from './images/waterCastel1280.jpg';
+
+const carouselImages = new Array();
+
+carouselImages[0] = new Image();
+carouselImages[0].src = architecture;
+
+carouselImages[1] = new Image();
+carouselImages[1].src = castelSantangel;
+
+carouselImages[2] = new Image();
+carouselImages[2].src = fairyTale;
+
+carouselImages[3] = new Image();
+carouselImages[3].src = neuschwanstein;
+
+carouselImages[4] = new Image();
+carouselImages[4].src = waterCastel;
+
+const carouselTrack = document.querySelector('.carousel__track');
+
+export default function displayImages(images) {
+  for (const image of images) {
+    const imageLineItem = document.createElement('li');
+  }
+}
